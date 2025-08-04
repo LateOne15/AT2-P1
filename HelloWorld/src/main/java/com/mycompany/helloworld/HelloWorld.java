8c63b91eee2792765fb9148add2f3e4732ca7e88
LateOne15 <48002046+LateOne15@users.noreply.github.com>
2025-08-04 12:38:10 +0800

    Init
    First Commit - Hello World

---
+++ HelloWorld/src/main/java/com/mycompany/helloworld/HelloWorld.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.helloworld;
+
+/**
+ *
+ * @author Administrator
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        if(1==1)
+        {
+            System.out.println("Hello World!");
+        }
+    }
+}
